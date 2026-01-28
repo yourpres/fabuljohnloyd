d965d43ede0d64070f81742f8731bc178cb1b8a0
yourpres <22-08416@g.batstate-u.edu.ph>
2026-01-28 11:39:09 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ function toggleDetail(id) {
         element.style.display = "block";
     }
 }
+document.getElementById('contactBtn').addEventListener('click', function(e) {
+    // This copies the email to the clipboard automatically
+    const email = "fabuljohn03@gmail.com";
+    navigator.clipboard.writeText(email).then(() => {
+        // Show a temporary message that it was copied
+        const msg = document.getElementById('copyMessage');
+        msg.style.display = 'block';
+        
+        // Hide the message after 3 seconds
+        setTimeout(() => {
+            msg.style.display = 'none';
+        }, 3000);
+    });
+});
