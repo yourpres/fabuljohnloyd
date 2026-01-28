e164d3c0166399eb60fe1257c2af1a6df21918af
yourpres <22-08416@g.batstate-u.edu.ph>
2026-01-28 10:28:24 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleDetail(id) {
+    const element = document.getElementById(id);
+    if (element.style.display === "block") {
+        element.style.display = "none";
+    } else {
+        element.style.display = "block";
+    }
+}
